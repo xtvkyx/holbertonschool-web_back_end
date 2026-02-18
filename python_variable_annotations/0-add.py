bce9f2e33415e0292a0f51995943a11ff690f2f9
“xtvkyx” <ljabli@icloud.com>
2026-02-18 19:55:49 +0000

    fix blank space

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,4 +5,3 @@
 def add(a: float, b: float) -> float:
     """Returns the sum of two float numbers."""
     return a + b
-
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,4 +5,3 @@
 def concat(str1: str, str2: str) -> str:
     """Returns the concatenation of two strings."""
     return str1 + str2
-
