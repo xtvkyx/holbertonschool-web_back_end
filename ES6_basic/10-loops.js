be15bd5af53e9b43839cc2e31a7601677967a97d
“xtvkyx” <ljabli@icloud.com>
2026-03-08 17:54:03 +0000

    9-12

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+	let index = 0;
+for (const value of array) {
+    array[idex] = appendString + value;
+	index++:
+  }
+
+  return array;
+}
