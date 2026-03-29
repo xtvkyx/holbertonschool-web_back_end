9ce466168fc6cdbfc5d4d2b843b27abf4c4c5635
“xtvkyx” <ljabli@icloud.com>
2026-03-29 12:02:12 +0000

    6-10

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (!startString) return '';
+
+  const result = [];
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+
+  return result.join('-');
+}
