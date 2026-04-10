a53666269722248069a30b30caf9e07c2859ed28
Lamisfaljabli <ljabli@icloud.com>
2026-04-10 16:39:59 +0300

    Refactor AppController and update response method

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,7 @@
-export default class AppController {
-  static getHomepage(req, res) {
-    res.status(200).send('Hello Holberton School!');
+class AppController {
+  static getHomepage(request, response) {
+    response.send(200, 'Hello Holberton School!');
   }
 }
+
+export default AppController;
